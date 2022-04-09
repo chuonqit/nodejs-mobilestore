3f364d6e1637b2d224a7cd1c137d92ee90dff4eb
ChuongQIT <chuonqit@gmail.com>
2022-04-09 19:25:34 +0700

    Access-Control-Allow-Origin middlewares

--- src/app.js
+++ src/app.js
@@ -22,6 +22,12 @@ import sliderRoute from "./routes/sliderRoute";
 
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // middlewares
 app.use(morgan("tiny"));
 app.use(express.json({ limit: "50mb" }));
